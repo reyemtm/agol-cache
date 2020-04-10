0c2e4cb76a1c88f84d19aca507b58b83b0fd60b4
reyemtm <malcolmtmeyer@gmail.com>
2020-04-10 06:36:36 -0400

    add option for folder

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const cache = require("./lib/featureServiceToGeoJSON.js")
 
 const urls = [
-  "https://services9.arcgis.com/IUhP9plEzDTayUVC/arcgis/rest/services/Muskingum_County_Benchmarks/FeatureServer/"
+  "https://services9.arcgis.com/IUhP9plEzDTayUVC/arcgis/rest/services/Muskingum_County_Benchmarks/FeatureServer/",
+  "https://services9.arcgis.com/IUhP9plEzDTayUVC/arcgis/rest/services/Water_System_View/FeatureServer/"
 ];
 
-cache(urls[0])
+cache(urls[1], {folder: "test"})

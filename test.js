a6ef60b8c0796386d3cd335b8437fc4ead1bef22
Malcolm Meyer <malcolmtmeyer@gmail.com>
2020-11-30 16:03:42 -0500

    attachments

--- test.js
+++ test.js
@@ -3,10 +3,14 @@ const cache = require("./lib/featureServiceToGeoJSON.js")
 const urls = [
   "https://utility.arcgis.com/usrsvcs/servers/76f8f4bd06084d4d9ad4efed1db30590/rest/services/zanesville_utility/MapServer/",
   "https://services9.arcgis.com/IUhP9plEzDTayUVC/arcgis/rest/services/Muskingum_County_Benchmarks/FeatureServer/",
-  "https://services9.arcgis.com/IUhP9plEzDTayUVC/arcgis/rest/services/Water_Utilities/FeatureServer/"
+  "https://services9.arcgis.com/IUhP9plEzDTayUVC/arcgis/rest/services/Water_Utilities_View_II/FeatureServer/"
 ];
 
 cache.featureServiceToGeoJSON(urls[1], {
-  debug: true,
+  debug: true
   // filter: "storm"
-})
+});
+
+// const queryAttachments = require("./lib/queryAttachments.js")
+
+// queryAttachments("https://services9.arcgis.com/IUhP9plEzDTayUVC/ArcGIS/rest/services/Water_Utilities_View_II/FeatureServer/", "../geojson-cache", "../images")

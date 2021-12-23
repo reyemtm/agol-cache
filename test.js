bd4b518b3a125b4d25eb4c2d2ee4791b3a6c1134
Meyer <mmeyer@ci.lancaster.oh.us>
2021-12-23 09:48:05 -0500

    added JSON endpoint, udpates to spinner

--- test.js
+++ test.js
@@ -1,29 +1,30 @@
 const cache = require("./lib/featureServiceToGeoJSON.js")
 const queryAttachments = require("./lib/queryAttachments.js")
 
+//NOTE the first URL will error out, so it provides a good test case.
 const urls = [
-  "https://utility.arcgis.com/usrsvcs/servers/76f8f4bd06084d4d9ad4efed1db30590/rest/services/zanesville_utility/MapServer/",
-  "https://services9.arcgis.com/IUhP9plEzDTayUVC/arcgis/rest/services/Muskingum_County_Benchmarks/FeatureServer/",
-  "https://services9.arcgis.com/IUhP9plEzDTayUVC/arcgis/rest/services/Water_Utilities_View_II/FeatureServer/",
-  "https://services9.arcgis.com/IUhP9plEzDTayUVC/arcgis/rest/services/Water_Utilities/FeatureServer/",
-  "https://services9.arcgis.com/IUhP9plEzDTayUVC/arcgis/rest/services/Zanesville_Brownfield_Map_WFL1/FeatureServer/"
+  "https://partnerships.nationalmap.gov/arcgis/rest/services/USGSLidarProjects/MapServer/",
+  "https://sampleserver6.arcgisonline.com/arcgis/rest/services/AGP/Hurricanes/MapServer",
+  "https://sampleserver6.arcgisonline.com/arcgis/rest/services/AGP/USA/MapServer"
 ];
 
 cache.featureServiceToGeoJSON(urls[2], {
-  attachments: true, //wheher or not to check the service for attachments, overridden by leaving esriIdField blank
-  debug: true, //debugging is now on be default, which just means it writes to a log file, and the console if silent is set to false 
-  esriIdField: "", //field to use for the esriIdField, used in the query parameters
+  attachments: false, //whether or not to check the service for attachments
+  debug: false, //debugging is now on be default, which just means it writes to a log file, and the console logger is off if silent is set to false 
+  esriIdField: "", //field to use for the esriIdField, used in the query parameters, if NULL it is determined by the service response
   filter: "", //string to filter layer names
-  folder: "geojson-cache", //folder to write the log file and geojson cache, relative to working directory or absolute path
-  layerByLayer: false, //use await on each layer, helpful for debugging
-  prefix: "", //prefix to add to the start of layer names
-  silent: true, //turn off viewing log messages in the console, disabled if debug is set to false, however spinner is always on
-  token: null, //token to use for secured routes, taken from .env TOKEN variable
-  pretty: false, //if true, the JSON output file will be formatted for human reading
+  folder: './geojson-cache', //folder to write the log file and geojson cache, relative to working directory or absolute path
+  format: "json", //json or GeoJSON - json downloads the raw Esri JSON format then converts to GeoJSON (BETA), try this if using the GeoJSON endpoint fails
+  layerByLayer: false, //use await on each layer, slower but helpful for debugging
+  prefix: "test_", //prefix to add to the start of layer names
+  silent: true, //turn off viewing winston log messages and spinner "info" messages in the console
+  timeout: 2000, //default is 5000, increase as needed
+  token: null //token to use for secured routes, taken from .env TOKEN variable
 }, getAttachments);
 
-function getAttachments() {
-  console.log("Attempting to query attachments.");
+function getAttachments(layers) {
+  console.log("\nThe callback function has access to the layers object:\n");
+  console.log(layers)
   process.exit()
   // queryAttachments("https://services9.arcgis.com/IUhP9plEzDTayUVC/ArcGIS/rest/services/Water_Utilities_View_II/FeatureServer/", "./geojson-cache")  
 }

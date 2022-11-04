aae730c1522d8cb86406779b7d5fbb1f0975e02a
Malcolm Meyer <malcolmtmeyer@gmail.com>
2022-11-04 10:07:23 -0400

    removed process.exit()

--- test.js
+++ test.js
@@ -1,31 +1,18 @@
 const cache = require("./lib/featureServiceToGeoJSON.js");
 const queryAttachments = require("./lib/queryAttachments.js");
 
-//NOTE the first URL will error out, so it provides a good test case.
-const urls = [
-  "https://partnerships.nationalmap.gov/arcgis/rest/services/USGSLidarProjects/MapServer/",
-  "https://sampleserver6.arcgisonline.com/arcgis/rest/services/AGP/Hurricanes/MapServer",
-  "https://sampleserver6.arcgisonline.com/arcgis/rest/services/AGP/USA/MapServer",
-  "https://gissd.sandag.org/rdw/rest/services/Zoning/Zoning/MapServer",
-  "https://webmaps.sandiego.gov/arcgis/rest/services/DSD/Zoning_Base/MapServer",
-  "https://services7.arcgis.com/feBzKmbMMjDq2yKj/ArcGIS/rest/services/NRT_Trail_Data_ESRI_web_2017_11_26/FeatureServer/",
-  "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS/USGS_Stream_Gauges/MapServer/", //stream gages
-  "https://services.gis.ca.gov/arcgis/rest/services/Transportation/HSR_OppZone/MapServer/", //CA Opportunity zones
-  "https://services3.arcgis.com/xTjSNSS8uiNdYSaW/ArcGIS/rest/services/epasld_places/FeatureServer/",
-];
-
 // async only works with layerByLayer: true
 (async () => {
-  const layers = await cache.featureServiceToGeoJSON(urls[2],
+  const layers = await cache.featureServiceToGeoJSON('https://services2.arcgis.com/3eV4Iyp0knHf3tfe/ArcGIS/rest/services/pptype090122/FeatureServer2',
     {
       attachments: false, //whether or not to check the service for attachments
       debug: true, //debugging is now on be default, which just means it writes to a log file, and the console logger is off if silent is set to false
       esriIdField: "", //field to use for the esriIdField, used in the query parameters, if NULL it is determined by the service response
-      filter: "states", //string to filter layer names
+      filter: "pptype090122", //string to filter layer names
       folder: "./output", //folder to write the log file and geojson cache, relative to working directory or absolute path
       format: "json", //json or GeoJSON - json downloads the raw Esri JSON format then converts to GeoJSON (BETA), try this if using the GeoJSON endpoint fails
       layerByLayer: true, //use await on each layer, slower but helpful for debugging
-      prefix: "cache_", //prefix to add to the start of layer names
+      prefix: "ppl_", //prefix to add to the start of layer names
       silent: false, //turn off viewing winston log messages and spinner "info" messages in the console
       timeout: 20000, //default is 5000, increase as needed
       token: null, //token to use for secured routes, taken from .env TOKEN variable
